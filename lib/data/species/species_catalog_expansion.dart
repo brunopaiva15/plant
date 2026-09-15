8eb1631935147b92e30cb4df84d4c0d853d40efd
Bruno Vergasta Paiva <ppaiva11144@gmail.com>
2026-09-15 12:10:32 +0200

    Wire Iris 1000 catalog into curated species

--- lib/data/species/species_catalog_expansion.dart
+++ lib/data/species/species_catalog_expansion.dart
@@ -2,6 +2,7 @@ import '../../domain/species/species_info.dart';
 import 'species_catalog_expansion_500.dart' as catalog500;
 import 'catalog_650/species_catalog_650.dart';
 import 'catalog_800/species_catalog_800.dart';
+import 'catalog_1000/species_catalog_1000.dart';
 
 /// Extensions du catalogue curaté.
 ///
@@ -12,5 +13,6 @@ abstract final class SpeciesCatalogExpansion {
     ...catalog500.SpeciesCatalogExpansion.entries,
     ...SpeciesCatalog650.entries,
     ...SpeciesCatalog800.entries,
+    ...SpeciesCatalog1000.entries,
   ];
 }
