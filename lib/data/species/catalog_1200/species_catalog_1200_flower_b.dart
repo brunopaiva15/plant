606e8edb5c4bc09db26240663e6aed7dee1783f5
Bruno Vergasta Paiva <ppaiva11144@gmail.com>
2026-09-15 12:20:28 +0200

    Add Iris species for catalog 1200 flowers B

---
+++ lib/data/species/catalog_1200/species_catalog_1200_flower_b.dart
@@ -0,0 +1,38 @@
+import '../../../domain/species/species_info.dart';
+
+/// Ajouts Iris 8 pour le palier 1 200 (fleurs B).
+abstract final class SpeciesCatalog1200FlowerB {
+  static const List<SpeciesCatalogEntry> entries = [
+    SpeciesCatalogEntry("Brunnera macrophylla", "Boraginaceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Brunnera sibirica", "Boraginaceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Calendula arvensis", "Asteraceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Callianthe megapotamica", "Malvaceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Callistephus chinensis", "Asteraceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Callitriche stagnalis", "Plantaginaceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Campanula glomerata", "Campanulaceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Campanula persicifolia", "Campanulaceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Campanula portenschlagiana", "Campanulaceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Campanula poscharskyana", "Campanulaceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Campsis grandiflora", "Bignoniaceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Campsis radicans", "Bignoniaceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Canna indica", "Cannaceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Canna × hybrida", "Cannaceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Carduus nutans", "Asteraceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Carduus tenuiflorus", "Asteraceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Carex flava", "Cyperaceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Carthamus lanatus", "Asteraceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Cirsium heterophyllum", "Asteraceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Cistus × purpureus", "Cistaceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Cleome houtteana", "Cleomaceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Coreopsis tinctoria", "Asteraceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Coreopsis verticillata", "Asteraceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Cortaderia selloana", "Poaceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Cosmos sulphureus", "Asteraceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Crepis capillaris", "Asteraceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Crepis foetida", "Asteraceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Crepis paludosa", "Asteraceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Crepis praemorsa", "Asteraceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Crocosmia × crocosmiiflora", "Iridaceae", SpeciesCategory.flower),
+    SpeciesCatalogEntry("Crocus chrysanthus", "Iridaceae", SpeciesCategory.flower),
+  ];
+}
