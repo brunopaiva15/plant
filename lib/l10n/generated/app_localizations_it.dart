2514dc5d1ea141b8683fc854dd7d034706c9c437
Bruno Vergasta Paiva <ppaiva11144@gmail.com>
2026-09-18 20:23:41 +0200

    feat(l10n): generate uncertain identification messages

--- lib/l10n/generated/app_localizations_it.dart
+++ lib/l10n/generated/app_localizations_it.dart
@@ -2040,6 +2040,16 @@ class AppLocalizationsIt extends AppLocalizations {
       'Una foglia, un fiore o la pianta intera aiuta a precisare.';
 
   @override
+  String get identificationUncertainTitle => 'Identificazione incerta';
+
+  @override
+  String get identificationUncertainBody =>
+      'Anche con le foto disponibili, nessuna specie emerge con sufficiente chiarezza. Puoi cercare online o scegliere manualmente se riconosci la pianta.';
+
+  @override
+  String get identificationSuggestionsToCheck => 'Suggerimenti da verificare';
+
+  @override
   String get identifyConfirmWithPhoto => 'Conferma con una foto';
 
   @override
